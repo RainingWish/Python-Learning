ab3f06dc4822fef2043d2e4b2068dd90fd22dfe3
RainingWish <51265465+RainingWish@users.noreply.github.com>
2019-06-02 20:53:14 -0400

    Add files via upload

---
+++ print.py
@@ -0,0 +1,9 @@
+#This file will print some stuff
+
+print('hello, world')
+
+print ('What', 'is', 'comma', 'do' ,'in', 'python')
+
+print ('100 + 200 =', 100+200)
+
+
---
+++ text.py
@@ -0,0 +1,16 @@
+print('linux basic command')
+
+print('create/edit a file => gedit')
+
+print('move a file => mv')
+
+print('enter a file => cd')
+
+print('exit a file => cd ..' )
+
+print('delet a file => rm')
+
+print('change a file name => mv oldname newname')
+
+print('run a pythin file => python3')
+
