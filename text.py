5ebe637e8c41e21482638181e9acbfaf5237a828
RainingWish <51265465+RainingWish@users.noreply.github.com>
2019-06-02 21:16:28 -0400

    Add files via upload

---
+++ text.py
@@ -0,0 +1,18 @@
+print('linux basic command')
+
+print('create/edit a file => gedit')
+
+print('move a file => mv')
+
+print('enter a file => cd')
+
+print('exit a file => cd ..' )
+
+print('delet a file => rm')
+
+print('change a file name => mv oldname newname')
+
+print('run a pythin file => python3')
+
+print('create a new folder in linux => mkdir')
+
